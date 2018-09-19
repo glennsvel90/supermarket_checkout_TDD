9fbdca81005f6266f1446370b796b3e1c03ce66d
glennsvel90 <glennsvelu@gmail.com>
2018-09-19 16:05:14 -0400

    test add item price

--- Checkout.py
+++ Checkout.py
@@ -1,3 +1,5 @@
 
 class Checkout:
-    pass
+
+    def addItemPrice(self, item, price):
+        pass
--- TestCheckout.py
+++ TestCheckout.py
@@ -5,3 +5,7 @@ from Checkout import Checkout
 class CheckoutTests(unittest.TestCase):
     def test_CanInstantiateCheckout(self):
         co = Checkout()
+
+    def test_CanAddItemPrice(self):
+        co = Checkout()
+        co.addItemPrice("a",1)
