8c5de4429ab847b2145d2e825cc94f0f223dea1c
glennsvel90 <glennsvelu@gmail.com>
2018-09-20 13:13:13 -0400

    test calculate total

--- Checkout.py
+++ Checkout.py
@@ -3,3 +3,9 @@ class Checkout:
 
     def addItemPrice(self, item, price):
         pass
+
+    def addItem(self, item):
+        pass
+
+    def CalculateTotal(self):
+        return 1
--- TestCheckout.py
+++ TestCheckout.py
@@ -1,11 +1,18 @@
-import unittest
+import pytest
 from Checkout import Checkout
 
+@pytest.fixture()
+def checkout():
+    checkout = Checkout()
+    return checkout
 
-class CheckoutTests(unittest.TestCase):
-    def test_CanInstantiateCheckout(self):
-        co = Checkout()
+def test_CanAddItemPrice(checkout):
+    checkout.addItemPrice("a",1)
 
-    def test_CanAddItemPrice(self):
-        co = Checkout()
-        co.addItemPrice("a",1)
+def test_CanAddItem(checkout):
+    checkout.addItem("a")
+
+def test_CanFindTheCurrentTotal(checkout):
+    checkout.addItemPrice("a", 1)
+    checkout.addItem("a")
+    assert checkout.CalculateTotal() == 1
