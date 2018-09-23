8011d0734da9ab57a7590248dbb834e5bc48fefa
glennsvel90 <glennsvelu@gmail.com>
2018-09-23 18:02:32 -0400

    calculate total function

--- Checkout.py
+++ Checkout.py
@@ -1,14 +1,42 @@
+import math
+
 
 class Checkout:
+
+    class Discount:
+        def __init__(self, numberofItems, price):
+            self.numberofItems = numberofItems
+            self.price = price
     def __init__(self):
         self.prices = {}
-        self.total = 0
+        self.discounts = {}
+        self.items = {}
+
+    def addDiscount(self, item, numberofItems, price):
+        discount = self.Discount(numberofItems,price)
+        self.discounts[item] = discount
 
     def addItemPrice(self, item, price):
         self.prices[item] = price
 
     def addItem(self, item):
-        self.total += self.prices[item]
+        if item in self.items:
+            self.items[item] += 1
+        else:
+            self.items[item] = 1
 
     def CalculateTotal(self):
-        return self.total
+        total = 0
+        for item, cnt in self.items.items():
+            if item in self.discounts:
+                discount = self.discounts[item]
+                if cnt >= discount.numberofItems:
+                    numofDiscounts = math.trunc(cnt/discount.numberofItems)
+                    total += numofDiscounts * discount.price
+                    remaining = cnt % discount.numberofItems
+                    total += remaining * self.prices[item]
+                else:
+                    total += cnt * self.prices[item]
+            else:
+                total += cnt * self.prices[item]
+        return total
--- TestCheckout.py
+++ TestCheckout.py
@@ -4,17 +4,28 @@ from Checkout import Checkout
 @pytest.fixture()
 def checkout():
     checkout = Checkout()
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
     return checkout
 
 
 def test_CanFindTheCurrentTotal(checkout):
-    checkout.addItemPrice("a", 1)
     checkout.addItem("a")
     assert checkout.CalculateTotal() == 1
 
 def test_GetCorrectTotalWithMultipleItems(checkout):
-    checkout.addItemPrice("a", 1)
-    checkout.addItemPrice("b", 2)
     checkout.addItem("a")
     checkout.addItem("b")
     assert checkout.CalculateTotal() == 3
+
+def test_AddDiscount(checkout):
+    checkout.addDiscount("b", 3, 2)
+
+
+
+def test_ApplyDiscount(checkout):
+    checkout.addDiscount("b", 3, 2)
+    checkout.addItem("b")
+    checkout.addItem("b")
+    checkout.addItem("b")
+    assert checkout.CalculateTotal() == 2
