e2c4a96e2a0a8fce21cefa6ac928ce2c55547bde
Glenn Velupillai <glennsvelu@gmail.com>
2018-09-24 19:06:00 -0400

    instructions

--- Checkout.py
+++ Checkout.py
@@ -2,8 +2,14 @@ import math
 
 
 class Checkout:
+    """ Class to represent an checkout object system
+    
+    Attributes:
+        
+    
 
     class Discount:
+        """Class to represent the discount for an item"""
         def __init__(self, numberofItems, price):
             self.numberofItems = numberofItems
             self.price = price
@@ -38,7 +44,6 @@ class Checkout:
             discount = self.discounts[item]
             if cnt >= discount.numberofItems:
                 total += self.calculateItemDiscountedTotal(item, cnt, discount)
-
             else:
                 total += cnt * self.prices[item]
         else:
@@ -52,3 +57,14 @@ class Checkout:
         remaining = cnt % discount.numberofItems
         total += remaining * self.prices[item]
         return total
+
+
+
+print("""
+Explore the docstring for the Checkout class to understand its features.
+To do so type: help(Checkout)
+You may create an instance of a  checkout system and then add items. 
+To create an instance of a checkout system, type into the terminal while in python interactive mode: system = Checkout()
+Then add items to the checkout system by typing: system.addItem()
+
+""")
